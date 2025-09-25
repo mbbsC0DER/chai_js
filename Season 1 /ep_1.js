1e54a321acb83bc6d0a8c39faeaf371877182d82
mbbsC0DER <kprathamnegi05@gmail.com>
2025-09-25 07:00:04 +0000

    variable 1 commit

--- Season 1 /ep_1.js
+++ Season 1 /ep_1.js
@@ -1,1 +1,11 @@
-console.log("Hello duniya")
+/*
+let and const use kar sakte 
+var use karna avoid karna chahiye (Kyuki var scope nhi samjhta ...)
+let me ham empty variable bhi bana sakte hai .
+
+*/
+var acc_name = "Pratham "
+var acc_no = "1223425"
+const account_id = 10
+let acc_state 
+console.table([acc_name , account_id , acc_no , acc_state])
