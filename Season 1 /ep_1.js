2d4f2e380f48c18e0aa8484dc82fb2cd8e76fe1a
mbbsC0DER <kprathamnegi05@gmail.com>
2025-09-25 18:03:14 +0000

    extensions commit

--- Season 1 /ep_1.js
+++ Season 1 /ep_1.js
@@ -1,11 +1,17 @@
 /*
+ecma scripts --> js ke docs 
+
 let and const use kar sakte 
 var use karna avoid karna chahiye (Kyuki var scope nhi samjhta ...)
 let me ham empty variable bhi bana sakte hai .
+alert(3 + 3) we use node js not browser !
 
 */
+"use strict" // treat all code as newer version
+
 var acc_name = "Pratham "
 var acc_no = "1223425"
 const account_id = 10
 let acc_state 
 console.table([acc_name , account_id , acc_no , acc_state])
+
