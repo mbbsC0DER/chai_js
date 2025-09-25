9dd7b97bd5c300ccfbdc11ad325fe2ef3f1ea959
mbbsC0DER <kprathamnegi05@gmail.com>
2025-09-25 18:17:03 +0000

    data types commit

--- Season 1 /ep_1.js
+++ Season 1 /ep_1.js
@@ -13,5 +13,28 @@ var acc_name = "Pratham "
 var acc_no = "1223425"
 const account_id = 10
 let acc_state 
-console.table([acc_name , account_id , acc_no , acc_state])
+// console.table([acc_name , account_id , acc_no , acc_state])
+
+/*
+DATA TYPES :
+
+number --> 2^53
+bigint
+string --> ""
+boolean --> true / false 
+null --> ek standalone value hai (empty ka type hai)
+undefined --> value nhi di hai abh tak 
+symbol (used more in react) --> for unique
+
+object
+*/
+console.log(typeof acc_name) //string 
+console.log(typeof null) //object 
+console.log(typeof undefined) // undefined 
+
+
+
+
+
+
 
