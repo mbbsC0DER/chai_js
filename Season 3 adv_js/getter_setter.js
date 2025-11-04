1fd97c4f4396c154e1a8b0d4a703a15d6a48fde5
mbbsC0DER <kprathamnegi05@gmail.com>
2025-11-04 15:44:40 +0530

    final commit

--- Season 3 adv_js/getter_setter.js
+++ Season 3 adv_js/getter_setter.js
@@ -1,3 +1,5 @@
+//using classes
+
 class user {
     constructor(name , age){
         this.name = name
@@ -22,3 +24,47 @@ class user {
 const obj = new user ( "pratham negi" , 20)
 console.log(obj._age)
 console.log(obj.name)
+
+//using functions 
+
+function user2(email , passwd) {
+    this._email = email
+    this._passwd = passwd
+    Object.defineProperty(this , "email" , {
+        get : function(){
+            return this._email.toUpperCase()
+        } , 
+        set : function(val) {
+            this._email = val
+        }
+    })
+    Object.defineProperty(this , "passwd" , {
+        get : function(){
+            return `${this._passwd}123 `
+        } , 
+        set : function(val) {
+            this._passwd = val
+        }
+    })
+}
+
+const property = new user2("negi05@gmail.com" , 890)
+console.log (property.email)
+console.log (property.passwd)
+
+//using objects
+
+const obj1 = {
+    _name : "taklu" , 
+    age : 20 , 
+
+    get name(){
+        return this._name.toUpperCase()
+    } ,
+    set name(val){
+        this._name = val
+    }
+}
+
+const obj2 = Object.create(obj1)    //obj2.__proto__ === obj1
+console.log (obj2.name) //yeh get name() ko call kar rha hai 
---
+++ Season 3 adv_js/lexical_scope.js
@@ -0,0 +1,27 @@
+//lexical scoping 
+function outer(){
+    let name = "pratham"
+    function inner(){
+        console.log ("Inner function : ",name)  //Inner function :  pratham
+    }
+    inner()
+}
+outer()
+// console.log("Outer function : " , name)       //=> error 
+
+
+
+//closure and
+//Eg 1 
+
+function outer_closure (){
+    let name = "negi"
+
+    function inner_closure(){
+        console.log ( "Inner closure : " , name)
+    }
+    return inner_closure
+}
+const store = outer_closure()   //fn ka pura lexical scope return hoga 
+store()     //Inner closure :  negi
+
